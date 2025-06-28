cdd6910418876c089cd77e53606870b670f75dc5
Vitaly Reshetyuk <vitaly.askz.maknii@gmail.com>
2025-06-28 21:17:50 +0200

    get output of `nm -C ...` with help of `exec(...)`

--- tests/bfd_test.cpp
+++ tests/bfd_test.cpp
@@ -1,5 +1,6 @@
 #include <bfd.h>
 #include <gtest/gtest.h>
+#include <stdio.h>
 
 import std;
 import types;
@@ -7,11 +8,41 @@ import bfd_wrapper_module;
 
 auto out = &std::cerr;
 
+Strings exec(StrView cmd) {
+    std::array<char, 1024> buffer;
+    std::string output;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(
+        popen(cmd.data(), "r"), pclose
+    );
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), static_cast<int>(buffer.size()), pipe.get()) !=
+           nullptr) {
+        output += buffer.data();
+    }
+
+    Strings result;
+    auto sstream{std::stringstream{output}};
+
+    for (std::string line; std::getline(sstream, line, '\n');) {
+        std::cout << line << '\n';
+    }
+
+    return result;
+}
+
 auto t() {
     auto lib{
         BfdWrapper{"/mnt/d/work/codebrowser_fork/index_libs/tests/data/"
                    "libLLVMDWARFLinker.a"}
     };
+
+    String cmd{"nm -C "};
+    auto checked{exec(cmd + lib.filename())};
+    std::ranges::for_each(checked, [](auto const &line) {
+        std::cout << line << '\n';
+    });
 
     *out << lib.filename() << ":\n";
     for (auto const &bfd : BfdRange(lib)) {
