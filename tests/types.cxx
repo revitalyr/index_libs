0e5f6c7f29740b382206c6b890f83567faa4d42f
Vitaly Reshetyuk <vitaly.askz.maknii@gmail.com>
2025-06-30 21:21:22 +0200

    `StringList`

--- tests/types.cxx
+++ tests/types.cxx
@@ -2,7 +2,8 @@ export module types;
 
 import std;
 
-export using String    = std::string;
-export using Strings   = std::vector<String>;
-export using StringSet = std::unordered_set<String>;
-export using StrView   = std::string_view;
+export using String     = std::string;
+export using Strings    = std::vector<String>;
+export using StringSet  = std::unordered_set<String>;
+export using StringList = std::forward_list<String>;
+export using StrView    = std::string_view;
